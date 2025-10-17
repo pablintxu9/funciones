037246cc47783724e25b6968aec685c57976f495
pablo <pablo.diazgrenufiguerolaferrtt@usp.ceu.es>
2025-10-17 11:14:02 +0200

    3

---
+++ factorial.py
@@ -0,0 +1,9 @@
+import math
+def fact(num):
+    if num<0:
+        return print("El factorial no está definido para números negativos")
+    elif num>0:
+        print("el numero seleccionado es :",num)
+        factorial=math.factorial(num)
+
+
